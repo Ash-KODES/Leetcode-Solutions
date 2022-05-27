d7d985ef7c0321566def3094f12794e6a72ab583
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-05-28 01:43:43 +0530

    Time: 2 ms (36.98%), Space: 6 MB (25.88%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) 
+    {
+        while(b!=0)
+        {
+            unsigned int temp=(a & b);
+            a=a^b;
+            b=(int)(temp<<1);
+        }
+        return a;
+    }
+};
