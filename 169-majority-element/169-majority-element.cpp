a0140c6d76ca4a73753f035e0c25f327f539d4ec
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-05-31 17:15:35 +0530

    Time: 25 ms (58.65%), Space: 19.5 MB (59.79%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) 
+    {
+      int element;
+      int count=0;
+      for(int i=0;i<nums.size();i++)
+      {
+        if(count==0)
+        element=nums[i];
+        if(element==nums[i])
+          count++;
+        else
+          count--;
+      }
+      return element;
+    }
+};
