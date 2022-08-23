7a62a81362b0c3fbcd0707e1969f182a2dbbc58c
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-08-23 22:36:14 +0530

    Time: 226 ms (8.72%), Space: 43.9 MB (30.45%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) 
     {
-       map<int,int> mp;
-       int sum=0;
-       int ans=0;
-       mp[sum]++;
-       for(int i=0;i<nums.size();i++)
-       {
-          sum+=nums[i];
-          if(mp.find(sum-k)!=mp.end())
-          ans+=mp[sum-k];
-          mp[sum]++;
-       }
-       return ans;
+       //Total number of subarray whose sum is k
+       //Brute Froce-Finding All the subarray---adding their elements--checking if its equal to k or not.
+       //O(n^3);
+       //Optimal:precomputation
+        map<int,int> mp;
+        mp[0]++;
+        int ans=0;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(mp.find(sum-k)!=mp.end())
+            {
+                ans+=mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return ans;
     }
 };
