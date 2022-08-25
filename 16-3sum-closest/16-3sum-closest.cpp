9767bf34db2018e1f9627cd5de7c130f4df4903c
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-08-26 00:38:18 +0530

    Time: 716 ms (33.93%), Space: 16.4 MB (61.42%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
+        //BRUTE FORCE-o(n^3)
+        //Optimal->sort then use two pointers
+        sort(nums.begin(),nums.end());
+        int diff=INT_MAX;
+        int ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            int lo=0;
+            int hi=nums.size()-1;
+            while(hi>lo)
+            {
+                if(hi==i)
+                {
+                    hi--;
+                    continue;
+                }
+                if(lo==i)
+                {
+                    lo++;
+                    continue;
+                }
+                int sum=nums[i]+nums[hi]+nums[lo];
+                if(abs(sum-target)<diff)
+                {
+                    ans=sum;
+                    diff=min(diff,abs(sum-target));
+                }
+                if(sum>target)
+                hi--;
+                else 
+                lo++;
+            }
+        }
+        return ans;
+        
+    }
+};
