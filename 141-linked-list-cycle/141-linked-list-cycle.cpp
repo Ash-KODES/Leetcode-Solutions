00b8f37dd2b2f0341d2a244394e74e919493359f
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-08-29 22:46:29 +0530

    Time: 11 ms (85.87%), Space: 8 MB (81.65%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -10,17 +10,15 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) 
     {
-        if(head==NULL)
-        return false;
-        ListNode *fast=head;
-        ListNode *slow=head;
-        while(fast&&fast->next)
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL&&fast->next!=NULL)
         {
             fast=fast->next->next;
             slow=slow->next;
             if(slow==fast)
             return true;
         }
-        return false;   
+        return false;
     }
 };
