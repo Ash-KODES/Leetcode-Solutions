c6b05c026f816dee93d19bb7ef85b311a0a80844
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-09-03 05:52:35 +0530

    Time: 39 ms (60.60%), Space: 22.3 MB (55.80%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -17,17 +17,20 @@ public:
         return nums[nums.size()-1];
         int hi=nums.size()-1;
         int lo=0;
-        while(hi-lo>=0)
+        while(hi-lo>1)
         {
             int mid=(lo+hi)/2;
-            if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1])
-            return nums[mid];
+            // if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1])
+            // return nums[mid];
             if(((mid%2)==0&&nums[mid]==nums[mid+1])||((mid%2)==1&&nums[mid]==nums[mid-1]))
             lo=mid+1;
             else
-            hi=mid-1;
+            hi=mid;
         }
-        
+        if(nums[lo]!=nums[lo+1]&&nums[lo]!=nums[lo-1])
+        return nums[lo];
+        if(nums[hi]!=nums[hi-1]&&nums[hi]!=nums[hi+1])
+        return nums[hi];
         return -1;
     }
 };
