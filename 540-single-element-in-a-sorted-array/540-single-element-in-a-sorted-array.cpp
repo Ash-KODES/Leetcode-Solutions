e1ba9d206db7428a505b99e0bd04da707355b99c
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-09-03 05:49:17 +0530

    Time: 33 ms (78.32%), Space: 22.3 MB (55.80%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -2,9 +2,32 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums)
     {
-        int xor_result=0;
-        for(int i=0;i<nums.size();i++)
-        xor_result^=nums[i];
-        return xor_result;   
+        //O(n)
+        // int xor_result=0;
+        // for(int i=0;i<nums.size();i++)
+        // xor_result^=nums[i];
+        // return xor_result;
+        //Optimal
+        //--------------------------------
+        if(nums.size()==1)
+        return nums[0];
+        if(nums[0]!=nums[1])
+        return nums[0];
+        if(nums[nums.size()-1]!=nums[nums.size()-2])
+        return nums[nums.size()-1];
+        int hi=nums.size()-1;
+        int lo=0;
+        while(hi-lo>=0)
+        {
+            int mid=(lo+hi)/2;
+            if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1])
+            return nums[mid];
+            if(((mid%2)==0&&nums[mid]==nums[mid+1])||((mid%2)==1&&nums[mid]==nums[mid-1]))
+            lo=mid+1;
+            else
+            hi=mid-1;
+        }
+        
+        return -1;
     }
 };
