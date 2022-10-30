5609486b7783f88d809335418491a517505974e1
ASHUTOSH KUMAR CHOUDHARY <choudharykumarashutosh1209@gmail.com>
2022-10-30 19:52:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (67.14%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+private:
+    int helper(int n,int k)
+    {
+        if(n==1)
+        return 0;
+        return (helper(n-1,k)+k)%n;
+    }
+public:
+    int findTheWinner(int n, int k)
+    {
+        return helper(n,k)+1;   
+    }
+};
